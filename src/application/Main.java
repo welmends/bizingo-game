21f3183f40b67f36e52c42005b931a0df0fd51e4
well <wellmend0@gmail.com>
2020-02-05 21:33:38 -0300

    add initial project

---
+++ src/application/Main.java
@@ -0,0 +1,34 @@
+package application;
+	
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+
+public class Main extends Application {
+	public static void main(String[] args) {        
+		launch(args);
+	}
+	 
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		Parent root = FXMLLoader.load(getClass().getResource("scenes/main_scene.fxml"));
+		primaryStage.setResizable(false);
+		//primaryStage.getIcons().add(new Image(this.getClass().getResourceAsStream("")));
+		primaryStage.setTitle("Bizingo Game");
+		primaryStage.setScene(new Scene(root, 1280, 720));
+		primaryStage.show();
+		
+		primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+		    @Override public void handle(WindowEvent t) {
+		        Platform.exit();
+		        System.exit(0);
+		    }
+		});
+	}
+}

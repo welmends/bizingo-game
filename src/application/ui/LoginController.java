4cdddc1c6cb6bced05942e3c2dc3a4fbcff1b765
well <wellmend0@gmail.com>
2020-02-06 23:02:37 -0300

    make fade from login to bizingo main game

---
+++ src/application/ui/LoginController.java
@@ -0,0 +1,77 @@
+package application.ui;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.animation.FadeTransition;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.util.Duration;
+
+public class LoginController implements Initializable {
+	
+	// FXML Variables
+	@FXML VBox loginVBox;
+	@FXML Label titleLabel;
+	@FXML Label ipLabel;
+	@FXML TextField ipTextField;
+	@FXML Label portLabel;
+	@FXML TextField portTextField;
+	@FXML Button connectButton;
+	
+	AnchorPane node1;
+	HBox node2;
+	
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		
+		
+		connectButton.addEventHandler(MouseEvent.MOUSE_PRESSED, new EventHandler<MouseEvent>(){
+
+	        @Override
+	        public void handle(MouseEvent event) {
+	        	makeFadeTransition();
+	        }
+		});
+	}
+	
+	public void loadFromParent(HBox mainHBox, AnchorPane loginAnchorPane) {
+		this.node1 = loginAnchorPane;
+		this.node2 = mainHBox;
+	}
+	
+	private void makeFadeTransition() {
+		FadeTransition fadeTransition1 = new FadeTransition(Duration.millis(500), node1);
+		FadeTransition fadeTransition2 = new FadeTransition(Duration.millis(500), node2);
+		node2.setVisible(false);
+		fadeTransition1.setFromValue(1);
+		fadeTransition1.setToValue(0);
+		fadeTransition1.setOnFinished(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+			    Platform.runLater(new Runnable() {
+			        @Override
+			        public void run() {
+			        }
+			    });
+			    node1.setVisible(false);
+				node2.setVisible(true);
+				node2.setOpacity(0);
+				fadeTransition2.setFromValue(0);
+				fadeTransition2.setToValue(1);
+				fadeTransition2.play();
+			}
+		});
+		fadeTransition1.play();
+	}
+}
--- src/application/ui/MainController.java
+++ src/application/ui/MainController.java
@@ -8,40 +8,53 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
 
 public class MainController implements Initializable {
 	
 	// FXML Variables
+	@FXML AnchorPane root;
 	@FXML HBox mainHBox;
+	@FXML AnchorPane loginAnchorPane;
 	
 	// FXML Loaders
+	FXMLLoader loginLoader;
 	FXMLLoader bizingoLoader;
 	FXMLLoader chatLoader;
 	
 	// Controllers
+	LoginController loginController;
 	BizingoController bizingoController;
 	ChatController chatController;
 	
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
+		loginLoader = new FXMLLoader(getClass().getResource("/application/scenes/login_scene.fxml"));
 		bizingoLoader = new FXMLLoader(getClass().getResource("/application/scenes/bizingo_scene.fxml"));
 		chatLoader = new FXMLLoader(getClass().getResource("/application/scenes/chat_scene.fxml"));
 		
+		Scene loginScene = null;
 		Scene bizingoScene = null;
 		Scene chatScene = null;
 		
 		try {
+			loginScene = new Scene(loginLoader.load());
 			bizingoScene = new Scene(bizingoLoader.load());
 			chatScene = new Scene(chatLoader.load());
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 		
+		
 		mainHBox.getChildren().add(bizingoScene.getRoot());
 		mainHBox.getChildren().add(chatScene.getRoot());
+		loginAnchorPane.getChildren().add(loginScene.getRoot());
 		
+		loginController = loginLoader.getController();
 		bizingoController = bizingoLoader.getController();
 		chatController = chatLoader.getController();
+		
+		loginController.loadFromParent(mainHBox, loginAnchorPane);
 	}
 }

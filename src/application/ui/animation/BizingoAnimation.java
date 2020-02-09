bb558890fd7146879da9aff9168ffc10a4eda071
well <wellmend0@gmail.com>
2020-02-09 11:44:59 -0300

    overload move method in BizingoAnimation

--- src/application/ui/animation/BizingoAnimation.java
+++ src/application/ui/animation/BizingoAnimation.java
@@ -46,4 +46,48 @@ public class BizingoAnimation {
 			}
 		});
 	}
+	
+	public void move(BizingoPiece piece, Double[] new_location) {
+		Double[] dest_location = new_location;
+		
+		st = new SequentialTransition();
+		timeline = new Timeline();
+		
+		kv_x = new KeyValue(piece.stack.layoutXProperty(), dest_location[0]-piece.radius);
+		kv_y = new KeyValue(piece.stack.layoutYProperty(), dest_location[1]-piece.radius);
+		kf = new KeyFrame(Duration.millis(millis), kv_x, kv_y);
+		
+		timeline.getKeyFrames().addAll(kf);
+		
+		st.getChildren().addAll(timeline);
+		st.play();
+		st.setOnFinished(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				piece.setPosition(dest_location);
+			}
+		});
+	}
+	
+	public void move(BizingoPiece piece, Double x, Double y) {
+		Double[] dest_location = new Double[]{x, y};
+		
+		st = new SequentialTransition();
+		timeline = new Timeline();
+		
+		kv_x = new KeyValue(piece.stack.layoutXProperty(), dest_location[0]-piece.radius);
+		kv_y = new KeyValue(piece.stack.layoutYProperty(), dest_location[1]-piece.radius);
+		kf = new KeyFrame(Duration.millis(millis), kv_x, kv_y);
+		
+		timeline.getKeyFrames().addAll(kf);
+		
+		st.getChildren().addAll(timeline);
+		st.play();
+		st.setOnFinished(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				piece.setPosition(dest_location);
+			}
+		});
+	}
 }

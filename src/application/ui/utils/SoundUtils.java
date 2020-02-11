f09cb68ff3233c26da210fb2a76b89aa69e62603
well <wellmend0@gmail.com>
2020-02-11 20:45:54 -0300

    create SoundUtils class

---
+++ src/application/ui/utils/SoundUtils.java
@@ -0,0 +1,44 @@
+package application.ui.utils;
+
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+
+public class SoundUtils {
+	
+	private Media capture;
+	private Media move;
+	private Media receive1;
+	private Media receive2;
+	
+	public SoundUtils() {
+		try {
+			capture  = new Media(getClass().getResource("/resources/sounds/capture.wav").toURI().toString());
+			move     = new Media(getClass().getResource("/resources/sounds/move.wav").toURI().toString());
+			receive1 = new Media(getClass().getResource("/resources/sounds/receive1.wav").toURI().toString());
+			receive2 = new Media(getClass().getResource("/resources/sounds/receive2.wav").toURI().toString());
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
+	
+	private void playSound(Media m) {
+        MediaPlayer mediaPlayer = new MediaPlayer(m);
+        mediaPlayer.play();
+	}
+	
+	public void playCaptureSound() {
+		playSound(capture);
+	}
+	
+	public void playMoveSound() {
+		playSound(move);
+	}
+	
+	public void playReceive1Sound() {
+		playSound(receive1);
+	}
+	
+	public void playReceive2Sound() {
+		playSound(receive2);
+	}
+}

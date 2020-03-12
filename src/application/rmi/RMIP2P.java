151c47199a928c3d4d3b6ca95f54db77135cf8dd
well <wellmend0@gmail.com>
2020-03-12 20:54:57 -0300

    remake strucuture to p2p implementation

--- src/application/rmi/RMIP2P.java
+++ src/application/rmi/RMIP2P.java
@@ -1,280 +1,110 @@
 package application.rmi;
 
-import java.net.*;
-import java.util.concurrent.Semaphore;
-import java.io.*;
+import java.rmi.Naming;
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
 
-public class RMIP2P extends Thread {
-	// X
-    private ServerSocket serverSocket = null;
-    private Socket socket = null;
-    
-    private DataInputStream input_stream  = null;
-    private DataOutputStream output_stream = null;
+public class RMIP2P extends UnicastRemoteObject implements RMIP2PInterface {
 
-    private String message_input= "";
-    private String message_output= "";
+	private static final long serialVersionUID = 0L;
+	public static RMIP2PInterface rmi_client;
+	
+    private String server_link, client_link;
     
     private String peer_type;
-    private int thread_action;
-    private Boolean is_connected;
-    
-    private Semaphore mutex;
-    
-    private String CHAT_CODEC;
-    private String GAME_CODEC;
-    private String SYS_CODEC;
-    
     private String ip;
     private int port;
     
-    // X
-    public RMIP2P(){
-    	this.peer_type = "";
-    	this.thread_action = 1;
-    	this.is_connected = false;
-    	
-    	mutex = new Semaphore(1);
-    	
-    	CHAT_CODEC = "#C$";
-    	GAME_CODEC = "#G$";
-    	SYS_CODEC = "#S$";
-    }
-    // X
-    public RMIP2P(String ip, int port){
-    	this.peer_type = "";
-    	this.thread_action = 1;
-    	this.is_connected = false;
-    	
-    	mutex = new Semaphore(1);
-    	
-    	CHAT_CODEC = "#C$";
-    	GAME_CODEC = "#G$";
-    	SYS_CODEC = "#S$";
-    	
-        this.ip   = ip;
-        this.port = port;
-    }
-	// X
-    public void setup(String ip, int port) {
-    	this.peer_type = "";
-        this.ip   = ip;
-        this.port = port;
-    }
-	// X
-    @Override
-    public void run(){
-    	if(thread_action==1) {
-    		wait_connection();
-    	}
-    	if(thread_action==2) {
-    		try {
-                while(true){
-                    String message_received = input_stream.readUTF();
-                    mutex.acquire();
-                    message_input = message_received;
-                    mutex.release();
-                }
-            } catch(Exception e) {
-                System.out.println(e);
-                is_connected = false;
-            }
-    	}
-    }
-	// X
-    public Boolean connect(){
+	public RMIP2P(String ip, int port) throws RemoteException{
+		super();
+		
+		this.peer_type = "";
+		this.ip = ip;
+		this.port = port;
+		
+		setup();
+	}
+	
+	public Boolean setup() {
+		server_link = "rmi://"+this.ip+":"+String.valueOf(this.port)+"/"+RMIConstants.BIZINGO_RMI_SERVER_NAME;
+		client_link = "rmi://"+this.ip+":"+String.valueOf(this.port)+"/"+RMIConstants.BIZINGO_RMI_CLIENT_NAME;
+		
         try {
-        	this.peer_type = "server";
-        	
-            serverSocket = new ServerSocket(port, 50, InetAddress.getByName(ip));
-            
-            return true;
-        } catch(Exception e_server){
-        	System.out.println(e_server);
-        	try {
-        		this.peer_type = "client";
-        		
-        		socket = new Socket(InetAddress.getByName(ip), port);
-        		
-        		input_stream = new DataInputStream(socket.getInputStream());
-        		output_stream = new DataOutputStream(socket.getOutputStream());
-        		
-        		thread_action=2;// Flag receive behavior
-        		
-        		is_connected = true;
-        		
-        		this.start();
-        		
-        		return true;
-        	} catch(Exception e_client) {
-        		System.out.println(e_client);
-        		return false;
-        	}
+        	if(Naming.list(server_link).length==0) {
+    			this.peer_type = "server";
+    		}
+        } catch(Exception e){
+        	System.out.println(e);
         }
-    }
-	// X
-    public Boolean disconnect(){
-    	try {
-			socket.close();
-			
-			if(isServer()) {
-				serverSocket.close();
-			}
+        
+        try {
+        	if(Naming.list(client_link).length==0) {
+    			this.peer_type = "client";
+    			server_link = "rmi://"+this.ip+":"+String.valueOf(this.port)+"/"+RMIConstants.BIZINGO_RMI_CLIENT_NAME;
+    			client_link = "rmi://"+this.ip+":"+String.valueOf(this.port)+"/"+RMIConstants.BIZINGO_RMI_SERVER_NAME;
+    		}
+        } catch(Exception e){
+        	System.out.println(e);
+        }
+		
+        if(this.peer_type.compareTo("")==0) {
+        	return false;
+        }
+        return true;
+	}
+	
+	public Boolean rebind() {
+		try {
+			Naming.rebind(server_link, this);
 			
 			return true;
-		} catch (Exception e) {
+		} catch(Exception e){
 			System.out.println(e);
+			
 			return false;
 		}
-    }
-	// X
-    public Boolean wait_connection() {
-    	try {
-            socket = serverSocket.accept();
-            serverSocket.close();
-            
-            input_stream = new DataInputStream(socket.getInputStream());
-            output_stream = new DataOutputStream(socket.getOutputStream());
-            
-            thread_action = 2;// Flag receive behavior
-            
-            is_connected = true;
-            
-            return true;
-    	} catch(Exception e) {
-    		System.out.println(e);
-    		return false;
-    	}
-
-    }
-	// X
-    public Boolean chatMessageStackFull() {
-    	String message_received = "";
+	}
+	
+	public Boolean unbind() {
 		try {
-			mutex.acquire();
-			message_received = message_input;
-	    	mutex.release();
-		} catch (Exception e) {
+			Naming.unbind(server_link);
+			
+			return true;
+		} catch(Exception e){
 			System.out.println(e);
+			
+			return false;
 		}
-
-    	if(message_received.length()>0) {
-    		if(CHAT_CODEC.equals(message_received.substring(0,CHAT_CODEC.length()))) {
-    			return true;
-    		}else {
-    			return false;
-    		}
-    	}else {
-    		return false;
-    	}
-    }
-	// X
-    public Boolean gameMessageStackFull() {
-    	String message_received = "";
+	}
+	
+	public Boolean lookup() {
 		try {
-			mutex.acquire();
-			message_received = message_input;
-	    	mutex.release();
-		} catch (Exception e) {
+			rmi_client = (RMIP2PInterface)Naming.lookup(client_link);
+			
+			return true;
+		} catch(Exception e){
 			System.out.println(e);
+			
+			return false;
 		}
-
-    	if(message_received.length()>0) {
-    		if(GAME_CODEC.equals(message_received.substring(0,GAME_CODEC.length()))) {
-    			return true;
-    		}else {
-    			return false;
-    		}
-    	}else {
-    		return false;
-    	}
-    }
-	// X
-    public Boolean sysMessageStackFull() {
-    	String message_received = "";
+	}
+	
+	public Boolean has_connection() {
 		try {
-			mutex.acquire();
-			message_received = message_input;
-	    	mutex.release();
-		} catch (Exception e) {
+			if(Naming.list(server_link).length>1) {
+				return true;
+			}else {
+				return false;
+			}
+		} catch(Exception e){
 			System.out.println(e);
+			
+			return false;
 		}
-
-    	if(message_received.length()>0) {
-    		if(SYS_CODEC.equals(message_received.substring(0,SYS_CODEC.length()))) {
-    			return true;
-    		}else {
-    			return false;
-    		}
-    	}else {
-    		return false;
-    	}
-    }
-	// X
-    public void sendChatMessage(String msg) {
-		try {
-			message_output = CHAT_CODEC + msg;
-			output_stream.writeUTF(message_output);
-			output_stream.flush();
-		} catch (Exception e) {
-			System.out.println(e);
-		}
-    }
-	// X
-    public void sendGameMessage(String msg) {
-		try {
-			message_output = GAME_CODEC + msg;
-			output_stream.writeUTF(message_output);
-			output_stream.flush();
-		} catch (Exception e) {
-			System.out.println(e);
-		}
-    }
-	// X
-    public void sendSysMessage(String msg) {
-		try {
-			message_output = SYS_CODEC + msg;
-			output_stream.writeUTF(message_output);
-			output_stream.flush();
-		} catch (Exception e) {
-			System.out.println(e);
-		}
-    }
-	// X
-    public String getMessage() {
-    	String message_received = "";
-		try {
-			mutex.acquire();
-			message_received = message_input;
-			message_input = "";
-	    	mutex.release();
-		} catch (Exception e) {
-			System.out.println(e);
-		}
-    	return message_received.substring(CHAT_CODEC.length(),message_received.length()); 
-    }
-	// X
-    public String getPeerType() {
-    	return this.peer_type;
-    }
-	// X
-    public Boolean isServer() {
-    	if(this.peer_type.equals("server")) {
-    		return true;
-    	}
-    	return false;
-    }
-	// X
-    public Boolean isClient() {
-    	if(this.peer_type.equals("client")) {
-    		return true;
-    	}
-    	return false;
-    }
-	// X
-    public Boolean isConnected() {
-    	return is_connected;
+	}
+	
+	@Override
+	public String hello(String msg) {
+		return msg;
     }
 }
-
-
--- src/application/rmi/RMIP2PInterface.java
+++ src/application/rmi/RMIP2PInterface.java
@@ -3,7 +3,7 @@ package application.rmi;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 
-public interface RMIClientInterface extends Remote {
+public interface RMIP2PInterface extends Remote {
 	
 	String hello(String msg) throws RemoteException;
   
